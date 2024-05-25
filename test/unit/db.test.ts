d16ea3054fca0bffc015f6430e41b819a75e6d98
augustine-a8 <logos.agbeko@gmail.com>
2024-05-25 19:32:59 +0000

    removed db connection test suite

--- test/unit/db.test.ts
+++
@@ -1,8 +0,0 @@
-describe("Database Connection Test Suite", () => {
-  it("should use the correct database url", () => {
-    const dbUrl = process.env.DATABASE_URL;
-    expect(dbUrl).toBe(
-      "mongodb+srv://augustine_a:CxTEB8gAOTJEPlgo@cluster0.beivtz8.mongodb.net/med-pharma-test?retryWrites=true&w=majority&appName=Cluster0"
-    );
-  });
-});
