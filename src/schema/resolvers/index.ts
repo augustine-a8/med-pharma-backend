5eb0cd196a58ea1b99da7c3fa60054f7ff17105e
augustine-a8 <logos.agbeko@gmail.com>
2024-05-27 12:25:09 +0000

    added consultation resolvers to graphql schema

--- src/schema/resolvers/index.ts
+++ src/schema/resolvers/index.ts
@@ -1,11 +1,14 @@
 import { userResolver } from "./user.resolver";
+import { consultationResolver } from "./consultation.resolver";
 
 const resolvers = {
   Query: {
     ...userResolver.Query,
+    ...consultationResolver.Query,
   },
   Mutation: {
     ...userResolver.Mutation,
+    ...consultationResolver.Mutation,
   },
 };
 
